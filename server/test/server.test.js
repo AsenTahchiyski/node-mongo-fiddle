a26f84f3966165cd81fdb0f58ff96a1981abf47e
Asen Tahchiyski <asen.andreev.tahchiyski@puzzel.com>
2018-11-12 17:20:30 +0200

    Add tests for PATCH /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectId(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }];
 
 beforeEach(done => {
@@ -118,22 +120,62 @@ describe('DELETE /todos/:id', () => {
         expect(todo).toNotExist;
         done();
       }).catch(e => done(e));
-    })
+    });
   });
 
   it('should return 404 if todo not found', done => {
     const hexId = new ObjectId().toHexString();
 
     request(app)
-      .delete(`/todos/${hexId}`)
-      .expect(404)
-      .end(done);
+    .delete(`/todos/${hexId}`)
+    .expect(404)
+    .end(done);
   });
 
   it('should return 404 if object id is invalid', done => {
     request(app)
-      .delete('/todos/123abc')
-      .expect(404)
+    .delete('/todos/123abc')
+    .expect(404)
+    .end(done);
+  });
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', done => {
+    const hexId = todos[0]._id.toHexString();
+    const text = 'Updated text';
+
+    request(app)
+    .patch(`/todos/${hexId}`)
+    .send({
+      completed: true,
+      text
+    })
+    .expect(200)
+    .expect(res => {
+      expect(res.body.todo.text).toBe(text);
+      expect(res.body.todo.completed).toBe(true);
+      expect(typeof res.body.todo.completedAt).toBe('number');
+    })
+    .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', done => {
+    const hexId = todos[1]._id.toHexString();
+    const text = 'Updated text!';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: false,
+        text
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(typeof res.body.todo.completedAt).toNotExist;
+      })
       .end(done);
   });
 });
