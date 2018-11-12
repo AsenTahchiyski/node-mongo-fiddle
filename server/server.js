01de1cf0f7ba2bb3f60f1e3cd10d5e0be2d64a65
Asen Tahchiyski <asen.andreev.tahchiyski@puzzel.com>
2018-11-12 15:35:54 +0200

    Add tests for DELETE todos/:id

--- server/server.js
+++ server/server.js
@@ -56,10 +56,10 @@ app.delete('/todos/:id', (req, res) => {
 
   Todo.findByIdAndRemove(id).then(todo => {
     if (!todo) {
-      return res.send(404).send();
+      return res.status(404).send();
     }
 
-    res.send(todo);
+  res.send({todo});
   }).catch(e => {
     res.status(400).send();
   });
